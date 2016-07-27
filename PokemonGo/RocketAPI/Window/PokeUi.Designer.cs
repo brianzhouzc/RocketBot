642c297cf5dd16b8edfcdbcddb6d8709e02dbb5f
danielbublil <danielbub123@gmail.com>
2016-07-27 23:34:37 +0300

    Add Item List with Discard menu

--- PokemonGo/RocketAPI/Window/PokeUi.Designer.cs
+++ PokemonGo/RocketAPI/Window/PokeUi.Designer.cs
@@ -28,39 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PokeUi));
             this.listView1 = new System.Windows.Forms.ListView();
             this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.item = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.count = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.discardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.discardAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.Location = new System.Drawing.Point(16, 13);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(411, 314);
+            this.listView1.Size = new System.Drawing.Size(547, 386);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 332);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Location = new System.Drawing.Point(16, 409);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(411, 19);
+            this.button1.Size = new System.Drawing.Size(774, 32);
             this.button1.TabIndex = 3;
             this.button1.Text = "Reload";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 30);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(136, 26);
+            this.toolStripMenuItem1.Text = "Transfer";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.item,
+            this.count});
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(571, 13);
+            this.listView2.Margin = new System.Windows.Forms.Padding(4);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(219, 386);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView2_MouseClick);
+            // 
+            // item
+            // 
+            this.item.Text = "Items";
+            this.item.Width = 100;
+            // 
+            // count
+            // 
+            this.count.Text = "Quantity";
+            this.count.Width = 133;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.discardToolStripMenuItem,
+            this.discardAllToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(157, 56);
+            // 
+            // discardToolStripMenuItem
+            // 
+            this.discardToolStripMenuItem.Name = "discardToolStripMenuItem";
+            this.discardToolStripMenuItem.Size = new System.Drawing.Size(156, 26);
+            this.discardToolStripMenuItem.Text = "Discard 10";
+            this.discardToolStripMenuItem.Click += new System.EventHandler(this.discardToolStripMenuItem_Click);
+            // 
+            // discardAllToolStripMenuItem
+            // 
+            this.discardAllToolStripMenuItem.Name = "discardAllToolStripMenuItem";
+            this.discardAllToolStripMenuItem.Size = new System.Drawing.Size(156, 26);
+            this.discardAllToolStripMenuItem.Text = "Discard All";
+            this.discardAllToolStripMenuItem.Click += new System.EventHandler(this.discardAllToolStripMenuItem_Click);
+            // 
             // PokeUi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(435, 363);
+            this.ClientSize = new System.Drawing.Size(803, 447);
+            this.Controls.Add(this.listView2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "PokeUi";
             this.Text = "PokeUi";
             this.Load += new System.EventHandler(this.PokeUi_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -69,5 +160,13 @@
 
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader item;
+        private System.Windows.Forms.ColumnHeader count;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem discardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem discardAllToolStripMenuItem;
     }
 }
--- PokemonGo/RocketAPI/Window/PokeUi.cs
+++ PokemonGo/RocketAPI/Window/PokeUi.cs
@@ -9,11 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using PokemonGo.RocketAPI.Enums;
 using PokemonGo.RocketAPI.GeneratedCode;
+using System.Net;
+using System.IO;
 
 namespace PokemonGo.RocketAPI.Window
 {
     public partial class PokeUi : Form
     {
+        private Client client;
         public PokeUi()
         {
             InitializeComponent();
@@ -22,7 +25,8 @@ namespace PokemonGo.RocketAPI.Window
 
         private void PokeUi_Load(object sender, EventArgs e)
         {
-            Execute();
+          Execute();
+            
         }
 
         public static ISettings ClientSettings;
@@ -31,7 +35,7 @@ namespace PokemonGo.RocketAPI.Window
         {
             button1.Enabled = false;
 
-            var client = new Client(ClientSettings);
+            client = new Client(ClientSettings);
 
             try
             {
@@ -54,57 +58,84 @@ namespace PokemonGo.RocketAPI.Window
                         .OrderByDescending(key => key.Cp);
                 var families = inventory.InventoryDelta.InventoryItems
                     .Select(i => i.InventoryItemData?.PokemonFamily)
-                    .Where(p => p != null && (int) p?.FamilyId > 0)
+                    .Where(p => p != null && (int)p?.FamilyId > 0)
                     .OrderByDescending(p => (int)p.FamilyId);
-                    
-                    
-                
-                
+               
+
                 var imageList = new ImageList { ImageSize = new Size(50, 50) };
                 listView1.ShowItemToolTips = true;
-
+                PopulateItemList();
                 foreach (var pokemon in pokemons)
                 {
-
-                    var pokemonImage = GetPokemonImage((int)pokemon.PokemonId);
-                    imageList.Images.Add(pokemon.PokemonId.ToString(),pokemonImage);
+                    Bitmap pokemonImage = null;
+                    await Task.Run(() =>
+                    {
+                        pokemonImage = GetPokemonImage((int)pokemon.PokemonId);
+                    });
+                    imageList.Images.Add(pokemon.PokemonId.ToString(), pokemonImage);
 
                     listView1.LargeImageList = imageList;
                     var listViewItem = new ListViewItem();
-                    listViewItem.SubItems.Add("Cp: " + pokemon.Cp);
+                    listViewItem.Tag = pokemon;
 
 
                     var currentCandy = families
-                        .Where(i => (int) i.FamilyId <= (int) pokemon.PokemonId)
-                        .Select(f=>f.Candy)
+                        .Where(i => (int)i.FamilyId <= (int)pokemon.PokemonId)
+                        .Select(f => f.Candy)
                         .First();
                     var currIv = Math.Round(Perfect(pokemon));
                     //listViewItem.SubItems.Add();
                     listViewItem.ImageKey = pokemon.PokemonId.ToString();
-                    listViewItem.Text = string.Format("{0}\nCp:{1}", pokemon.PokemonId, pokemon.Cp);
-                    listViewItem.ToolTipText = "Candy: " + currentCandy+"\n"+"IV: "+currIv+"%";
+
+                    listViewItem.Text = string.Format("{0}\n{1} CP", pokemon.PokemonId, pokemon.Cp);
+                    listViewItem.ToolTipText = currentCandy + " Candy\n" + currIv + "% IV";
 
 
                     this.listView1.Items.Add(listViewItem);
 
 
                 }
+               
                 button1.Enabled = true;
 
             }
-            catch (TaskCanceledException){ Execute(); }
+            catch (TaskCanceledException) { Execute(); }
             catch (UriFormatException) { Execute(); }
-            catch (ArgumentOutOfRangeException) {  Execute(); }
+            catch (ArgumentOutOfRangeException) { Execute(); }
             catch (ArgumentNullException) { Execute(); }
             catch (NullReferenceException) { Execute(); }
-            catch (Exception ex) {  Execute(); }
+            catch (Exception ex) { Execute(); }
         }
-
+        private async void PopulateItemList()
+        {
+            listView2.Items.Clear();
+            var inventory = await client.GetInventory();
+            var items = inventory.InventoryDelta.InventoryItems.Select(i => i.InventoryItemData?.Item)
+                       .Where(p => p != null && p?.Count > 1).OrderByDescending(key => key.Item_);
+            int counter = 0;
+            foreach (Item item in items)
+            {
+                ListViewItem lvi = new ListViewItem(Convert.ToString((AllEnum.ItemId)item.Item_));
+                lvi.Tag = item;
+                listView2.Items.Add(lvi);
+                counter += item.Count;
+                lvi.SubItems.Add(item.Count.ToString());
+            }
+            listView2.Columns[0].Text = "Items (Total:" + counter.ToString() + ")";
+        }
         private static Bitmap GetPokemonImage(int pokemonId)
         {
-            var url = "http://pokeapi.co/media/sprites/pokemon/" + pokemonId + ".png";
+            var Sprites = AppDomain.CurrentDomain.BaseDirectory + "Sprites\\";
+            string location = Sprites + pokemonId + ".png";
+            if (!Directory.Exists(Sprites))
+                Directory.CreateDirectory(Sprites);
+            if (!File.Exists(location))
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadFile("http://pokeapi.co/media/sprites/pokemon/" + pokemonId + ".png", @location);
+            }
             PictureBox picbox = new PictureBox();
-            picbox.Load(url);
+            picbox.Image = Image.FromFile(location);
             Bitmap bitmapRemote = (Bitmap)picbox.Image;
             return bitmapRemote;
         }
@@ -117,13 +148,67 @@ namespace PokemonGo.RocketAPI.Window
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //if (listView1.SelectedItems.Count == 0) return;
-            //label1.Text = listView1.SelectedItems[0]?.SubItems[1].Text;
-            //label2.Text = listView1.SelectedItems[0]?.SubItems[2].Text;
+
         }
         public static float Perfect(PokemonData poke)
         {
             return ((float)(poke.IndividualAttack + poke.IndividualDefense + poke.IndividualStamina) / (3.0f * 15.0f)) * 100.0f;
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (listView1.FocusedItem.Bounds.Contains(e.Location) == true)
+                {
+                    contextMenuStrip1.Show(Cursor.Position);
+                }
+            }
+        }
+
+        private async void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var pokemon = (PokemonData)listView1.SelectedItems[0].Tag;
+
+
+            if (MessageBox.Show(this, pokemon.PokemonId + " with " + pokemon.Cp + " CP thats " + Math.Round(Perfect(pokemon)) + "% perfect", "Are you sure you want to transfer?", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                var transfer = await client.TransferPokemon(pokemon.Id);
+            }
+            listView1.Items.Remove(listView1.SelectedItems[0]);
+        }
+
+        private async void discardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var item = (Item)listView2.SelectedItems[0].Tag;
+             
+            var transfer = await client.RecycleItem((AllEnum.ItemId)item.Item_, 10);
+            PopulateItemList();
+        }
+
+        private async void discardAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var item = (Item)listView2.SelectedItems[0].Tag;
+            var transfer = await client.RecycleItem((AllEnum.ItemId)item.Item_, item.Count);
+            PopulateItemList();
+        }
+
+
+
+        private void listView2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (listView2.FocusedItem.Bounds.Contains(e.Location) == true)
+                {
+                    var item = (Item)listView2.SelectedItems[0].Tag;
+                    if (item.Count <= 10)
+                        discardToolStripMenuItem.Enabled = false;
+                    else
+                        discardToolStripMenuItem.Enabled = true;
+                    contextMenuStrip2.Show(Cursor.Position);
+                }
+            }
         }
     }
 }
