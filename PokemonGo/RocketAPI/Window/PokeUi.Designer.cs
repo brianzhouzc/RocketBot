ef9dd3b989a2e6857644a269c0e9f7b5f8c053b8
Ignacio <laborde.ignacio@gmail.com>
2016-07-27 19:10:53 -0300

    fix spelling error

--- PokemonGo/RocketAPI/Window/PokeUi.Designer.cs
+++ PokemonGo/RocketAPI/Window/PokeUi.Designer.cs
@@ -87,9 +87,9 @@
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(13, 19);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(82, 17);
+			this.label1.Size = new System.Drawing.Size(75, 17);
 			this.label1.TabIndex = 4;
-			this.label1.Text = "Pokemons: ";
+			this.label1.Text = "Pokemon: ";
 			// 
 			// lblPokemonsCount
 			// 
