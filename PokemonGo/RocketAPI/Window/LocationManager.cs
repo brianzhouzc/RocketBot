566d6e30d932579248cf2edca1e49c562bf77d6f
bozoweed <bozoweed@gmail.com>
2016-07-30 20:55:49 +0200

    FIX AND ADD FEATURE
    fix bag full cleared by recycler bug
    add info status bar ( time to level up , itemi in bag ) maybe latter total pokemon catched
    add pokeui DataGridView 
    fix error area and country on login
    add later list item with posibility to remove some item manualy

--- PokemonGo/RocketAPI/Window/LocationManager.cs
+++ PokemonGo/RocketAPI/Window/LocationManager.cs
@@ -25,7 +25,7 @@ namespace PokemonGo.RocketAPI.Window
 
         public async Task update(double lat, double lng)
         {
-            double waitTime = getDistance(lat, lng)/this.kilometersPerMillisecond;
+            double waitTime = getDistance(lat, lng) / this.kilometersPerMillisecond;
             await Task.Delay((int)Math.Ceiling(waitTime));
             await client.UpdatePlayerLocation(lat, lng);
         }
