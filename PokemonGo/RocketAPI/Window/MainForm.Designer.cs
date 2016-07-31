0ea5293974eec7035e88fe9ffaa35d614a66c673
Brian <brian1461748123@gmail.com>
2016-07-30 21:22:53 -0700

    fix typo

--- PokemonGo/RocketAPI/Window/MainForm.Designer.cs
+++ PokemonGo/RocketAPI/Window/MainForm.Designer.cs
@@ -56,8 +56,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.statusStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.objectListView1)).BeginInit();
@@ -325,8 +325,18 @@
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(330, 216);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Incomming Feature";
+            this.tabPage1.Text = "Incoming Feature";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Incoming Features";
             // 
             // tabPage2
             // 
@@ -337,16 +347,6 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = ":3";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(58, 105);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(206, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Incomming Features";
             // 
             // MainForm
             // 
