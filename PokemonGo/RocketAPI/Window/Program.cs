7db63ed8a417c74c6d1c2b64b5c1730610e0784c
Brian <brian1461748123@gmail.com>
2016-08-01 19:33:58 -0700

    Revert "Attempt to add autoupdate"
    This reverts commit ab784f92d4d675be6e4bc66d833d9889d08ef4ca.

--- PokemonGo/RocketAPI/Window/Program.cs
+++ PokemonGo/RocketAPI/Window/Program.cs
@@ -1,11 +1,8 @@
-﻿using KnightsWarriorAutoupdater;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
 
 namespace PokemonGo.RocketAPI.Window
 {
@@ -17,62 +14,9 @@ namespace PokemonGo.RocketAPI.Window
         [STAThread]
         static void Main()
         {
-            checkUpdate();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
-
-        static private void checkUpdate()
-        {
-            #region check and download new version program
-            bool bHasError = false;
-            IAutoUpdater autoUpdater = new AutoUpdater();
-            try
-            {
-                autoUpdater.Update();
-            }
-            catch (WebException exp)
-            {
-                MessageBox.Show("Can not find the specified resource");
-                bHasError = true;
-            }
-            catch (XmlException exp)
-            {
-                bHasError = true;
-                MessageBox.Show("Download the upgrade file error");
-            }
-            catch (NotSupportedException exp)
-            {
-                bHasError = true;
-                MessageBox.Show("Upgrade address configuration error");
-            }
-            catch (ArgumentException exp)
-            {
-                bHasError = true;
-                MessageBox.Show("Download the upgrade file error");
-            }
-            catch (Exception exp)
-            {
-                bHasError = true;
-                MessageBox.Show("An error occurred during the upgrade process");
-            }
-            finally
-            {
-                if (bHasError == true)
-                {
-                    try
-                    {
-                        autoUpdater.RollBack();
-                    }
-                    catch (Exception)
-                    {
-                        //Log the message to your file or database
-                    }
-                }
-            }
-            #endregion
-        }
     }
-
 }
