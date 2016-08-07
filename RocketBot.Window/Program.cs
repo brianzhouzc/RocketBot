ac97f818d7f34d13689d0fb18166dfc7bba43fab
Brian <brian1461748123@gmail.com>
2016-08-06 21:04:20 -0700

    Implement MainForm ui
    no logic yet

--- RocketBot.Window/Program.cs
+++ RocketBot.Window/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PokemonGo.RocketAPI.Window;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace RocketBot.Window
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
