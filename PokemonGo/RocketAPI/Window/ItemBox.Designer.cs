7459d47a5cded105b1414fa44111c80b6a89ea38
CJB <cjbok@ziggo.nl>
2016-08-10 20:46:09 +0200

    Added Item Manager and Pokemon Rename
    - Items can be used from the Item Manager
    - Recycle Items
    - Lucky egg now needs to be start from the item manager.

---
+++ PokemonGo/RocketAPI/Window/ItemBox.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PokemonGo.RocketAPI.Window {
+    partial class ItemBox {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lbl = new System.Windows.Forms.Label();
+            this.pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl.BackColor = System.Drawing.Color.Transparent;
+            this.lbl.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.lbl.Location = new System.Drawing.Point(0, 64);
+            this.lbl.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(64, 24);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "999";
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pb
+            // 
+            this.pb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb.BackColor = System.Drawing.Color.Transparent;
+            this.pb.Location = new System.Drawing.Point(0, 0);
+            this.pb.Margin = new System.Windows.Forms.Padding(0);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(64, 64);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb.TabIndex = 0;
+            this.pb.TabStop = false;
+            // 
+            // ItemBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.pb);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "ItemBox";
+            this.Size = new System.Drawing.Size(64, 88);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.Label lbl;
+    }
+}
---
+++ PokemonGo/RocketAPI/Window/ItemForm.cs
@@ -0,0 +1,27 @@
+﻿using POGOProtos.Inventory.Item;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokemonGo.RocketAPI.Window {
+    public partial class ItemForm : Form {
+        public ItemForm(ItemData item) {
+            InitializeComponent();
+
+            pb.Image = (Image)Properties.Resources.ResourceManager.GetObject(item.ItemId.ToString());
+            numCount.Maximum = item.Count;
+
+            if (item.ItemId == ItemId.ItemLuckyEgg || item.ItemId == ItemId.ItemIncenseOrdinary) {
+                btnRecycle.Text = "Use";
+                //btnRecycle.Enabled = false;
+                numCount.Visible = false;
+            }
+        }
+    }
+}
