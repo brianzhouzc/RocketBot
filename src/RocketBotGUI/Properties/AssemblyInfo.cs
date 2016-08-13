3a0a478df148f26f336fc986c5e7caea29eb977b
Brian <brian1461748123@gmail.com>
2016-08-13 02:18:28 -0700

    Update AssemblyVersion

--- src/RocketBotGUI/Properties/AssemblyInfo.cs
+++ src/RocketBotGUI/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.8.2.3")]
+[assembly: AssemblyVersion("1.8.2.4")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
