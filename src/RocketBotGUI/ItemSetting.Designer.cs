310a93d990d756ad0953feed435903467482b34d
Brian <brian1461748123@gmail.com>
2016-08-12 22:16:56 -0700

    Change font

--- src/RocketBotGUI/ItemSetting.Designer.cs
+++ src/RocketBotGUI/ItemSetting.Designer.cs
@@ -31,7 +31,7 @@
             // count
             // 
             this.count.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.count.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.count.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.count.Location = new System.Drawing.Point(132, 0);
             this.count.Maximum = new decimal(new int[] {
             999,
@@ -39,7 +39,7 @@
             0,
             0});
             this.count.Name = "count";
-            this.count.Size = new System.Drawing.Size(75, 22);
+            this.count.Size = new System.Drawing.Size(75, 21);
             this.count.TabIndex = 0;
             this.count.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -47,23 +47,23 @@
             // 
             this.ItemId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ItemId.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ItemId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ItemId.Location = new System.Drawing.Point(-1, -1);
             this.ItemId.Name = "ItemId";
-            this.ItemId.Size = new System.Drawing.Size(127, 20);
+            this.ItemId.Size = new System.Drawing.Size(127, 18);
             this.ItemId.TabIndex = 1;
             this.ItemId.Text = "Pokeball";
             this.ItemId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // ItemSetting
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.ItemId);
             this.Controls.Add(this.count);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "ItemSetting";
-            this.Size = new System.Drawing.Size(210, 21);
+            this.Size = new System.Drawing.Size(210, 19);
             ((System.ComponentModel.ISupportInitialize)(this.count)).EndInit();
             this.ResumeLayout(false);
 
