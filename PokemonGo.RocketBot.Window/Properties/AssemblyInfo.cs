b025b112466df34c77d2c70bac157f31d668473e
Brian <brian1461748123@gmail.com>
2016-08-14 02:55:50 -0700

    Change icon & Move screenshots to seperate folder

--- PokemonGo.RocketBot.Window/Properties/AssemblyInfo.cs
+++ PokemonGo.RocketBot.Window/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("NecroBot Client for Pokémon GO")]
+[assembly: AssemblyTitle("RocketBot 2.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
