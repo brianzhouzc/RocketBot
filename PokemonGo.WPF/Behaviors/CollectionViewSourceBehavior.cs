c31f774378f99a177c71e4a77826e4d099c699a6
wertzui <wertzui@spielebeet.de>
2016-08-26 16:14:29 +0200

    Sorting of Pokemon in Inventory

---
+++ PokemonGo.WPF/Behaviors/CollectionViewSourceBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PokemonGo.WPF.Behaviors
+{
+    public static class CollectionViewSourceBehavior
+    {
+        public static readonly DependencyProperty SortPropertyProperty =
+            DependencyProperty.RegisterAttached(
+                "SortProperty",
+                typeof(string),
+                typeof(CollectionViewSourceBehavior),
+                new UIPropertyMetadata("", OnSortPropertyChanged));
+
+        public static object GetSortProperty(DependencyObject element)
+        {
+            return element.GetValue(SortPropertyProperty);
+        }
+
+        public static void SetSortProperty(DependencyObject element, object value)
+        {
+            element.SetValue(SortPropertyProperty, value);
+        }
+
+        public static void OnSortPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                var collectionViewSource = dependencyObject as CollectionViewSource;
+                if (collectionViewSource == null)
+                {
+                    return;
+                }
+
+                var SortProperty = e.NewValue.ToString();
+                var newSortDescription = new SortDescription
+                {
+                    PropertyName = SortProperty
+                };
+                collectionViewSource.SortDescriptions.Clear();
+                collectionViewSource.SortDescriptions.Add(newSortDescription);
+            }
+        }
+    }
+}
