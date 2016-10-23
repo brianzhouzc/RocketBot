f09d622a4a90eb5d25b9a6ae8f06b826af140698
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-10-23 02:32:17 +0200

    Update Git

--- PokemonGo.RocketBot.Window/Properties/AssemblyInfo.cs
+++ PokemonGo.RocketBot.Window/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
 [assembly: AssemblyFileVersion("2.0.0")]
