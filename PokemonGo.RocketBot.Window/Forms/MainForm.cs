59cd616cac9d3bf2918f892b1ebef44ae8fc95b6
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-11-10 18:05:58 +0100

    Updated to android (0.45.0) and ios (1.15.0)

--- PokemonGo.RocketBot.Window/Forms/MainForm.cs
+++ PokemonGo.RocketBot.Window/Forms/MainForm.cs
@@ -6,7 +6,6 @@ using System.Drawing.Drawing2D;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
