1481245a50671a80fdffb5789f7ea203008986c8
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-12-24 18:13:49 +0100

    Update POGOProtos to 2.4.0 | 0.51.0 | 1.21.0 (https://github.com/AeonLucid/POGOProtos/pull/258)

--- PokemonGo.RocketBot.Window/Forms/MainForm.cs
+++ PokemonGo.RocketBot.Window/Forms/MainForm.cs
@@ -183,12 +183,6 @@ namespace PokemonGo.RocketBot.Window.Forms
             _session.EventDispatcher.EventReceived += evt => listener.Listen(evt, _session);
             _session.EventDispatcher.EventReceived += evt => aggregator.Listen(evt, _session);
 
-            if (_settings.UseWebsocket)
-            {
-                var websocket = new WebSocketInterface(_settings.WebSocketPort, _session);
-                _session.EventDispatcher.EventReceived += evt => websocket.Listen(evt, _session);
-            }
-
             _machine.SetFailureState(new LoginState());
             Logger.SetLoggerContext(_session);
 
