c412b21a16ffe62af4ff25ae861c8c71f678a309
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-12-25 02:08:40 +0100

    rename window

---
+++ NecroBot2/Program.cs
@@ -0,0 +1,20 @@
+﻿#region using directives
+
+using System;
+using System.Windows.Forms;
+using NecroBot2.Forms;
+
+#endregion
+
+namespace NecroBot2
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new MainForm());
+        }
+    }
+}
