f1303d7d7f3995f304ac3fd2276602b7dd6a9a6a
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-01-15 14:23:17 +0100

    Logic v1.0.0.62

---
+++ PoGo.NecroBot.Logic/Event/Snipe/AutoSnipePokemonAddedEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.Logic.Event.Snipe
+{
+    public class AutoSnipePokemonAddedEvent : IEvent
+    {
+        public AutoSnipePokemonAddedEvent(EncounteredEvent data)
+        {
+            this.EncounteredEvent = data;
+        }
+
+        public EncounteredEvent EncounteredEvent { get; set; }
+    }
+}
