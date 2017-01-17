8b9d0f01fc17fffde0f4545d5fa4677aeb00db29
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-01-17 22:24:20 +0100

    Rename Project

--- NecroBot2/RunWithExcelConfig.bat
+++
@@ -1,2 +0,0 @@
-﻿echo off
-Necrobot2.exe -provider excel
---
+++ RocketBot2/WebSocketHandler/GetCommands/Tasks/GetPokemonListTask.cs
@@ -0,0 +1,33 @@
+﻿#region using directives
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RocketBot2.WebSocketHandler.GetCommands.Events;
+using RocketBot2.WebSocketHandler.GetCommands.Helpers;
+using PoGo.NecroBot.Logic.State;
+using SuperSocket.WebSocket;
+using PoGo.NecroBot.Logic.Model;
+
+#endregion
+
+namespace RocketBot2.WebSocketHandler.GetCommands.Tasks
+{
+    internal class GetPokemonListTask
+    {
+        public static async Task Execute(ISession session, WebSocketSession webSocketSession, string requestID)
+        {
+            //using (var blocker = new BlockableScope(session, BotActions.ListItems))
+            {
+                //if (!await blocker.WaitToRun()) return;
+
+                var allPokemonInBag = await session.Inventory.GetHighestsCp(1000);
+                var families = await session.Inventory.GetPokemonFamilies();
+                var settings = await session.Inventory.GetPokemonSettings();
+                var list = new List<PokemonListWeb>();
+                allPokemonInBag.ToList().ForEach(o => list.Add(new PokemonListWeb(o, families, settings)));
+                webSocketSession.Send(EncodingHelper.Serialize(new PokemonListResponce(list, requestID)));
+            }
+        }
+    }
+}
