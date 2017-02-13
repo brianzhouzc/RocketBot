882fc51ecd007398ecc82d8f2829894292bea8ee
msntc <msntc@DESKTOP-BC9QJD0>
2017-02-13 11:56:01 +0000

    2.6.0.20

--- RocketBot2/Properties/AssemblyInfo.cs
+++ RocketBot2/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.0.33")]
+// [assembly: AssemblyVersion("2.6.0.20")]
 
-[assembly: AssemblyVersion("2.6.0.17")]
+[assembly: AssemblyVersion("2.6.0.20")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v2.6.0.17")]
+[assembly: AssemblyInformationalVersion("2.6.0.20")]
