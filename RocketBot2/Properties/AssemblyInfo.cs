10d32ddcdf14e32c0b7672d9f131289d50d18875
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-02-18 19:07:07 +0100

    v2.7.0.2

--- RocketBot2/Properties/AssemblyInfo.cs
+++ RocketBot2/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.7.0.2")]
+[assembly: AssemblyVersion("2.7.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v2.7.0.2")]
+[assembly: AssemblyInformationalVersion("v2.7.0.1")]
