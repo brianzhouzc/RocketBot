555f8a1f9425b65f445f88554e18e0bef8927c68
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-04-01 22:57:43 +0200

    minor fix

--- RocketBot2/Forms/MainForm.cs
+++ RocketBot2/Forms/MainForm.cs
@@ -389,6 +389,7 @@ namespace RocketBot2.Forms
 
         private async void gMapControl1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
+            if (!_botStarted) return;
             var pos = gMapControl1.FromLocalToLatLng(e.Location.X, e.Location.Y);
             await SetMoveToTargetTask.Execute(pos.Lat, pos.Lng);
         }
