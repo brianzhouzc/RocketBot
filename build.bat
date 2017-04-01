cbf4954434df4cbaa5533bc13ec92dcd638605ef
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2017-04-01 23:23:46 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 @echo off
-
 nuget.exe restore "RocketBot.sln"
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "RocketBot.sln" /property:Configuration="Release RocketBot" /property:Platform="Any CPU"
