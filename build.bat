f20a6b842c967964c712d80909ddf5a9abdb20a6
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-04-13 17:09:34 +0200

    Remove deprecated options.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 nuget.exe restore "RocketBot.sln"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "RocketBot.sln" /property:Configuration="Release RocketBot" /property:Platform="Any CPU"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MsBuild.exe" "RocketBot.sln" /property:Configuration="Release RocketBot" /property:Platform="Any CPU"
