22d68b5e579ebc2ff27cf1bc0c708e0e8cc0cf77
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-04-19 14:01:08 +0200

    Update nuget for vs2017

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@
 nuget.exe restore "RocketBot.sln"
 for /f "delims=" %%i in ('dir /s /b /a-d "%programfiles(x86)%\MSBuild.exe"') do (set RocketBuilder="%%i")
 %RocketBuilder% "RocketBot.sln" /property:Configuration="Release RocketBot" /property:Platform="Any CPU"
+set RocketBuilder=
