1b14a61ee79f844d80b7c3cab83d5f91c512b61a
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-04-24 06:46:15 +0200

    fix incubators walked km

--- RocketBot2/Forms/ItemBox.cs
+++ RocketBot2/Forms/ItemBox.cs
@@ -5,6 +5,11 @@ using POGOProtos.Inventory.Item;
 using RocketBot2.Helpers;
 using PoGo.NecroBot.Logic.Tasks;
 using PoGo.NecroBot.Logic.State;
+using System.Globalization;
+using POGOProtos.Inventory;
+using System.Threading.Tasks;
+using PoGo.NecroBot.Logic.Event;
+using System.Linq;
 
 namespace RocketBot2.Forms
 {
@@ -16,6 +21,7 @@ namespace RocketBot2.Forms
         public static ISession Session;
         public Incubators incubator;
         public Eggs egg;
+        public float kmWalked;
 
         public ItemBox(int see, int cath, Image pic)
         {
@@ -37,9 +43,9 @@ namespace RocketBot2.Forms
             DisableTimer = true;
             lbl.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             pb.Image = item.Icon;
-            lblTime.Text = $"{item.TotalKM:0.0}Km";
+            lblTime.Text = String.Format(CultureInfo.InvariantCulture, "{0:0.0}Km", item.TotalKM);
             lblTime.Visible = true;
-            lbl.Text = $"{item.KM:0.0}Km";
+            lbl.Text = String.Format(CultureInfo.InvariantCulture, "{0:0.0}Km", item.KM);
             lblTime.Parent = pb;
             egg = item;
             
@@ -51,15 +57,22 @@ namespace RocketBot2.Forms
             }
         }
 
+        public async Task GetPlayerStats()
+        {
+            var playerStats = (await Session.Inventory.GetPlayerStats().ConfigureAwait(false)).FirstOrDefault();
+            kmWalked = playerStats.KmWalked;
+        }
+
         public ItemBox(Incubators item)
         {
             InitializeComponent();
             DisableTimer = true;
             lbl.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            GetPlayerStats().ConfigureAwait(false);
             pb.Image = item.Icon(item.IsUnlimited);
-            lblTime.Text = $"{item.TotalKM - item.KM:0.0}Km";
+            lblTime.Text = String.Format(CultureInfo.InvariantCulture, "{0:0.0}Km", item.TotalKM - item.KM);
             lblTime.Visible = true;
-            lbl.Text = $"{item.TotalKM / 1000:0.0}Km";
+            lbl.Text = String.Format(CultureInfo.InvariantCulture,"{0:0.0}Km", kmWalked - item.KM);
             lblTime.Parent = pb;
             incubator = item;
  
