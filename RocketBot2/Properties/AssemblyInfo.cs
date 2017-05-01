78546e6bad7d07a7a750d918b63fdb1c2eefb871
Furtif <msntchat@hotmail.fr>
2017-05-01 02:18:44 -0700

    v2.9.1.18

--- RocketBot2/Properties/AssemblyInfo.cs
+++ RocketBot2/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.9.1.17")]
+// [assembly: AssemblyVersion("2.9.1.18")]
 
-[assembly: AssemblyVersion("2.9.1.17")]
+[assembly: AssemblyVersion("2.9.1.18")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v2.9.1.17")]
+[assembly: AssemblyInformationalVersion("v2.9.1.18")]
