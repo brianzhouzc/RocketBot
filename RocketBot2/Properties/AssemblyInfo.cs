5695f1959f0f475de6ff9b18dc077446d28c2bab
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2017-05-03 00:05:21 +0200

    Revert for stable version

--- RocketBot2/Properties/AssemblyInfo.cs
+++ RocketBot2/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.9.1.21")]
 
-[assembly: AssemblyVersion("2.9.1.21")]
+[assembly: AssemblyVersion("2.9.1.19")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v2.9.1.21")]
+[assembly: AssemblyInformationalVersion("v2.9.1.19")]
