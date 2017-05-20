5e91756a37ece74650650d7fd2c51c2e0a390756
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2017-05-20 20:54:04 +0200

    Update AssemblyInfo.cs

--- RocketBot2/Properties/AssemblyInfo.cs
+++ RocketBot2/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.0.405")]
+// [assembly: AssemblyVersion("2.10.1.2")]
 
-[assembly: AssemblyVersion("0.0.0.405")]
+[assembly: AssemblyVersion("2.10.1.2")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0.405")]
+[assembly: AssemblyInformationalVersion("v2.10.1.2")]
