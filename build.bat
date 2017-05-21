20fcf04f353dab4a645a08af52a5b14e1d2e209d
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-05-21 23:32:16 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
+cd %CD%
 nuget.exe restore "RocketBot.sln"
 for /f "delims=" %%i in ('dir /s /b /a-d "%programfiles(x86)%\MSBuild.exe"') do (set RocketBuilder="%%i")
 %RocketBuilder% "RocketBot.sln" /property:Configuration="Release RocketBot" /property:Platform="Any CPU"
 set RocketBuilder=
+pause
