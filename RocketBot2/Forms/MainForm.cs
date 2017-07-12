4eea67f6272463ddb7aad24d001cc0e35d466f78
--=FurtiF™=-- <msntchat@hotmail.fr>
2017-07-12 05:45:54 +0200

    fix bug

--- RocketBot2/Forms/MainForm.cs
+++ RocketBot2/Forms/MainForm.cs
@@ -404,7 +404,6 @@ namespace RocketBot2.Forms
                             if (marker.Position == pokeStopLoc)
                             {
                                 _pokestopsOverlay.Markers.Remove(marker);
-                                marker.Dispose();
                             }
                         }
                     }
