f1ce4d0d6ee3c096b1e489424c208a34267c33dd
TheWizard1328 <the.wizard@live.ca>
2017-07-20 12:37:26 -0600

    Correction to last commit.

--- RocketBot2/Forms/MainForm.cs
+++ RocketBot2/Forms/MainForm.cs
@@ -537,14 +537,22 @@ namespace RocketBot2.Forms
                 // Sets current location 
                 double Dist = LocationUtils.CalculateDistanceInMeters(_session.Client.CurrentLatitude, _session.Client.CurrentLongitude, pos.Lat, pos.Lng);
 
+                var lastPosFile = Path.Combine(_settings.ProfileConfigPath, "LastPos.ini");
+                if (File.Exists(lastPosFile))
+                {
+                    File.Delete(lastPosFile);
+                }
+
                 _session.Client.Settings.DefaultLatitude = pos.Lat;
                 _session.Client.Settings.DefaultLongitude = pos.Lng;
 
                 _settings.LocationConfig.DefaultLatitude = pos.Lat;
                 _settings.LocationConfig.DefaultLongitude = pos.Lng;
 
-                _session.Client.CurrentLatitude = pos.Lat;
-                _session.Client.CurrentLongitude = pos.Lng;
+                _session.Client.Player.SetCoordinates(pos.Lat, pos.Lng, _session.ElevationService.GetElevation(pos.Lat, pos.Lng).Result);
+
+                _settings.LocationConfig.DefaultLatitude = pos.Lat;
+                _settings.LocationConfig.DefaultLongitude = pos.Lng;
 
                 var newLocation = new PointLatLng(pos.Lat, pos.Lng);
                 GMapControl1.Position = newLocation;
@@ -555,7 +563,7 @@ namespace RocketBot2.Forms
 
                 _settings.Save(Path.Combine(Path.Combine(Directory.GetCurrentDirectory(), "Config"), "config.json"));
 
-                Logger.Write($"New starting location has been set to: Lat: {pos.Lat:0.00000000} Long: {pos.Lng:0.00000000} Dist: {Dist:0.00}m",LogLevel.Info);
+                Logger.Write($"New starting location has been set to: Lat: {pos.Lat:0.00000000} Long: {pos.Lng:0.00000000} Dist: {Dist:0.00}m", LogLevel.Info);
                 return;
             }
             await SetMoveToTargetTask.Execute(pos.Lat, pos.Lng);
