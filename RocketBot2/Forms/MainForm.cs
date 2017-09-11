573e81910f21ba83dc61ad5ee564d36dafad8b41
TheWizard1328 <the.wizard@live.ca>
2017-09-10 22:44:17 -0600

    Code Testing

--- RocketBot2/Forms/MainForm.cs
+++ RocketBot2/Forms/MainForm.cs
@@ -559,7 +559,7 @@ namespace RocketBot2.Forms
                         Points = _session.Navigation.WalkStrategy.Points;
                         _playerLocations.Clear();
                         _playerRouteOverlay.Routes.Clear();
-                        _playerOverlay.Routes.Clear();
+                        //_playerOverlay.Routes.Clear();
                         List<PointLatLng> routePointLatLngs = new List<PointLatLng>();
                         foreach (var item in Points)
                         {
